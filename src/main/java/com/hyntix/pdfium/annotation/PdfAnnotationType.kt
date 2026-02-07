602c70689bef21bff427fc4c463ad5d549fc48b7
beittu <vescemarco@gmail.com>
2026-02-07 21:57:16 +0100

    Define annotation type enumeration

---
+++ src/main/java/com/hyntix/pdfium/annotation/PdfAnnotationType.kt
@@ -0,0 +1,15 @@
+enum class PdfAnnotationType {
+    TEXT,
+    LINK,
+    FREE_TEXT,
+    LINE,
+    SQUARE,
+    POLYGON,
+    POLYLINE,
+    HSQUARE,
+    Highlight,
+    UNDERLINE,
+    STRIKEOUT,
+    Squiggly,
+    STAMP
+}
